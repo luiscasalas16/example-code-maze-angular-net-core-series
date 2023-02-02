33868deb52a9eb29c86e23aa6ff8678a48a358d9
Luis Carlos Salas Villalobos <lusalas16@gmail.com>
2023-02-01 23:34:57 -0600

    ejemplo

---
+++ README.md
@@ -0,0 +1,13 @@
+# code-maze-angular-net-core-series
+
+Proyecto de ejemplo de frontend en Angular y backend en .Net Core. El código es el resultado de realizar los tutoriales de las siguientes referencias:
+- (https://code-maze.com/net-core-series/)
+- (https://code-maze.com/angular-series/)
+
+Se ajustó la aplicación para que utilizara base de datos MSSQL.
+
+Para ejecutar el ejemplo se debe:
+- Generar la base de datos con el archivo "database.sql".
+- Actualizar el connection strings correspondiente en el archivo "brackend\AccountOwnerServer\appsettings.json".
+- Ejecutar la aplicación de backend.
+- Ejecutar la aplicación de frontend.
---
+++ brackend/AccountOwnerServer/Extensions/ServiceExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Contracts;
+using LoggerService;
+using Microsoft.EntityFrameworkCore;
+using Entities;
+using Repository;
+
+namespace AccountOwnerServer.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+        }
+
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+        }
+
+        public static void ConfigureLoggerService(this IServiceCollection services) 
+        {
+            services.AddSingleton<ILoggerManager, LoggerManager>(); 
+        }
+
+        public static void ConfigureMySqlContext(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<RepositoryContext>(options =>
+            {
+                options.UseSqlServer("name=ConnectionStrings:DefaultContext");
+            });
+        }
+
+        public static void ConfigureRepositoryWrapper(this IServiceCollection services) 
+        { 
+            services.AddScoped<IRepositoryWrapper, RepositoryWrapper>(); 
+        }
+    }
+}
